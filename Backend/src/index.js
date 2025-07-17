d08b36340c3bf0e41979f29fdefb6c53eba9f942
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-07-17 20:37:45 +0530

    corrected the index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,19 +4,18 @@ import cookieParser from "cookie-parser";
 import cors from "cors"; // cors error resolve - different port or domains for frontend and backend
 
 import path from "path";
-import { fileURLToPath } from 'url';
 
-import {connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js"; // for local files we need to put .js bcoz we are using type - module 
 import messageRoutes from "./routes/message.route.js";
-import { app , server } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT=process.env.PORT;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 
 // Increase the payload size limits for handling image uploads
 app.use(express.json({ limit: '50mb' })); // Increased from default ~100kb to 50mb
@@ -24,8 +23,8 @@ app.use(express.urlencoded({ limit: '50mb', extended: true })); // Also handle U
 
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true,
+  origin: "http://localhost:5173",
+  credentials: true,
 }));
 
 app.use("/api/auth", authRoutes);
@@ -34,10 +33,11 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
+
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
