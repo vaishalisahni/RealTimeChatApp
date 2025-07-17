5d9d107c6a8cd6bec0f820eea5bbbb5e3778ebb2
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-07-17 20:07:25 +0530

    checking error

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -26,18 +26,18 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // app.get("/*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-server.listen(PORT,()=>{
-    console.log(`Server is running on port: ${PORT}`);
-    connectDB();
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
