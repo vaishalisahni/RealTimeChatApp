0029f74cf72d71a3efe14098dc2f3d54bc8b2adc
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-07-17 21:11:03 +0530

    routes update

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -5,7 +5,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/users",protectRoute, getUsersForSidebar);
-router.get("/:id",protectRoute,getMessages);
+router.get("/conversation/:id",protectRoute,getMessages);
 
 router.post("/send/:id", protectRoute , sendMessage);
 
--- Frontend/src/store/useChatStore.js
+++ Frontend/src/store/useChatStore.js
@@ -25,7 +25,7 @@ export const useChatStore = create((set, get) => ({
   getMessages: async (userId) => {
     set({ isMessagesLoading: true });
     try {
-      const res = await axiosInstance.get(`/messages/${userId}`);
+      const res = await axiosInstance.get(`/messages/conversation/${userId}`);
       set({ messages: res.data });
     } catch (error) {
       toast.error(error.response.data.message);
