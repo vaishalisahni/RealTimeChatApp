282c2bafe7e08be81c2c945c93cb0df525b9d622
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-07-18 19:24:58 +0530

    cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const PORT = process.env.PORT||5001;
 const __dirname = path.resolve();
 
-console.log("NODE_ENV:", process.env.NODE_ENV);
+// console.log("NODE_ENV:", process.env.NODE_ENV);
 
 // Increase the payload size limits for handling image uploads
 app.use(express.json({ limit: '50mb' })); // Increased from default ~100kb to 50mb
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true })); // Also handle U
 
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://realtimechatapp-ffz4.onrender.com"],
+  origin: "http://localhost:5173",
   credentials: true,
 }));
 
