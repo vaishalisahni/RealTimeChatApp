69f1519c302e27cfa49f68e7f6e8922c5664b0ef
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-07-18 19:14:35 +0530

    path -corrected

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -32,13 +32,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
 
-  app.get("/.*/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
   });
 }
-
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
