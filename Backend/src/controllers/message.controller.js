5f824657628acfcd904dc3544b082283cda0eeb4
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-07-18 18:01:32 +0530

    corrected some typos

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js"
 import Message from "../models/message.model.js";
 import cloudinary from "../lib/cloudinary.js";
-import { getRecieverSocketId, io } from "../lib/socket.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -59,7 +59,7 @@ export const sendMessage = async (req, res) => {
         await newMessage.save();
 
         // real time functionality => socket.io
-        const receiverSocketId = getRecieverSocketId(receiverId);
+        const receiverSocketId = getReceiverSocketId(receiverId);
         if (receiverSocketId) {
             io.to(receiverSocketId).emit("newMessage", newMessage);
         }
