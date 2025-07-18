eab064574a87ce491f258a2b1fec0e841dd523ce
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-07-18 19:28:26 +0530

    path

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
 
